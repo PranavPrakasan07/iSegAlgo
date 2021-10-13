29d4065fad6cc2377e0fe87529565b9c3f341c2f
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-10-13 09:24:09 +0530

    Fixed nesting

--- GaussianBlurParallel.py
+++ GaussianBlurParallel.py
@@ -7,7 +7,7 @@ import datetime
 import imageio
 
 a = datetime.datetime.now()
-# pymp.config.nested = True
+pymp.config.nested = True
 face = imageio.imread('lion.pgm')
 
 print(face.shape)
@@ -24,16 +24,16 @@ j = None
 
 print("Breakpoint 1")
 
-# with pymp.Parallel(2) as p1:
-#     with pymp.Parallel(2) as p2:
-#         for i in p1.range(0, l):
-#             for j in p2.range(0, b):
-#                 padded[i+1][j+1] = face[i][j]
+with pymp.Parallel(2) as p1:
+    with pymp.Parallel(2) as p2:
+        for i in p1.range(0, l):
+            for j in p2.range(0, b):
+                padded[i+1][j+1] = face[i][j]
 
-with pymp.Parallel(4) as p2:
-    for i in p2.range(0, l):
-        for j in p2.range(0, b):
-            padded[i+1][j+1] = face[i][j]
+# with pymp.Parallel(4) as p2:
+#     for i in p2.range(0, l):
+#         for j in p2.range(0, b):
+#             padded[i+1][j+1] = face[i][j]
 
 print("Breakpoint 2")
 
@@ -45,22 +45,21 @@ j = None
 print("Breakpoint 3")
 
 
-# with pymp.Parallel(2) as p1:
-#     with pymp.Parallel(2) as p2:
-#         for i in p1.range(1, l+1):
-#             for j in p2.range(1, b+1):
-#                 res[i-1][j-1] = (convx[0][0]*padded[i-1][j-1] + convx[0][1]*padded[i-1][j]+convx[0][2]*padded[i-1][j+1] +
-#                                  convx[1][0]*padded[i][j-1]+convx[1][1]*padded[i][j] + convx[1][2]*padded[i][j+1] +
-#                                  convx[2][0]*padded[i+1][j-1] + convx[2][1]*padded[i+1][j] + convx[2][2]*padded[i+1][j+1])
+with pymp.Parallel(2) as p1:
+    with pymp.Parallel(2) as p2:
+        for i in p1.range(1, l+1):
+            for j in p2.range(1, b+1):
+                res[i-1][j-1] = (convx[0][0]*padded[i-1][j-1] + convx[0][1]*padded[i-1][j]+convx[0][2]*padded[i-1][j+1] +
+                                 convx[1][0]*padded[i][j-1]+convx[1][1]*padded[i][j] + convx[1][2]*padded[i][j+1] +
+                                 convx[2][0]*padded[i+1][j-1] + convx[2][1]*padded[i+1][j] + convx[2][2]*padded[i+1][j+1])
 
 
-with pymp.Parallel(4) as p2:
-    for i in p2.range(1, l+1):
-        for j in p2.range(1, b+1):
-            res[i-1][j-1] = (convx[0][0]*padded[i-1][j-1] + convx[0][1]*padded[i-1][j]+convx[0][2]*padded[i-1][j+1] +
-                                convx[1][0]*padded[i][j-1]+convx[1][1]*padded[i][j] + convx[1][2]*padded[i][j+1] +
-                                convx[2][0]*padded[i+1][j-1] + convx[2][1]*padded[i+1][j] + convx[2][2]*padded[i+1][j+1])
-
+# with pymp.Parallel(2) as p2:
+#     for i in p2.range(1, l+1):
+#         for j in p2.range(1, b+1):
+#             res[i-1][j-1] = (convx[0][0]*padded[i-1][j-1] + convx[0][1]*padded[i-1][j]+convx[0][2]*padded[i-1][j+1] +
+#                                 convx[1][0]*padded[i][j-1]+convx[1][1]*padded[i][j] + convx[1][2]*padded[i][j+1] +
+#                                 convx[2][0]*padded[i+1][j-1] + convx[2][1]*padded[i+1][j] + convx[2][2]*padded[i+1][j+1])
 
 print("Breakpoint 4")
 
